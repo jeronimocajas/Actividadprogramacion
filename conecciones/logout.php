8cf538b43bbb587abd7064413c73e910f01d3185
jeronimocajas <jeronimocajas5930@gmail.com>
2023-11-04 18:07:01 -0500

    programacion

---
+++ conecciones/logout.php
@@ -0,0 +1,11 @@
+<?php
+// Inicia la sesión (asegúrate de hacerlo en todos los archivos que necesiten sesión)
+session_start();
+
+// Destruye la sesión actual
+session_destroy();
+
+// Redirige al usuario a la página de inicio
+header("Location: ../index.php");
+exit;
+?>
