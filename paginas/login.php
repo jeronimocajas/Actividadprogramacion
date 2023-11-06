2ef6f9589767daddb47fefc54d57053a9e770b5e
jeronimocajas <jeronimocajas5930@gmail.com>
2023-11-05 20:39:19 -0500

    xccc

--- paginas/login.php
+++ paginas/login.php
@@ -2,97 +2,77 @@
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Recupera el nombre de usuario y contraseña del formulario
     $username = $_POST['usuario'];
     $password = $_POST['contraseña'];
 
-    // Conecta a la base de datos (ajusta los valores a tu configuración)
     require '../conecciones/conexion.php';
 
-    // Escapa los valores para evitar inyección SQL
     $username = mysqli_real_escape_string($conexion, $username);
     $password = mysqli_real_escape_string($conexion, $password);
 
-    // Consulta para verificar las credenciales del usuario
-    $query = "SELECT * FROM usuario WHERE nombre = '$username' AND constraseña = '$password'";
+    $query = "SELECT * FROM usuario WHERE nombre = '$username' AND contraseña = '$password'";
     $result = mysqli_query($conexion, $query);
 
     if (mysqli_num_rows($result) == 1) {
-        // Las credenciales son correctas, inicia sesión
         $_SESSION['usuario_autenticado'] = true;
         $_SESSION['nombre'] = $username;
-        // Opcional: También puedes guardar el ID del usuario en la sesión
-
-        // Redirige al usuario a la página de inicio
-        header('Location: ../index.php'); // Ajusta la ubicación a tu página principal
+        header('Location: ../index.php');
     } else {
-        // Las credenciales son incorrectas, muestra un mensaje de error
         $error_message = "Credenciales incorrectas, intenta nuevamente.";
     }
 
-    // Cierra la conexión a la base de datos
     mysqli_close($conexion);
 }
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>S.S.A</title>
-    
-    <!-- Required meta tags -->
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-      
-  <section class="vh-100">
-    <div class="container py-5 h-100">
-      <div class="row d-flex align-items-center justify-content-center h-100">
-        <div class="col-md-8 col-lg-7 col-xl-6">
-          <img src="./imagenes/mejor.png" class="img-fluid" alt="Phone image">
-        </div>
-        <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
-          <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-            <!-- Nombre de usuario input -->
-            <div class="form-outline mb-4">
-              <input type="text" id="usuario" class="form-control form-control-lg" name="usuario" />
-              <label class="form-label" for="usuario">User name</label>
-            </div>
-    
-            <!-- Contraseña input -->
-            <div class="form-outline mb-4">
-              <input type="password" id="contraseña" class="form-control form-control-lg" name="contraseña" />
-              <label class="form-label" for="contraseña">Password</label>
-            </div>
+    <title>Iniciar Sesión</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
 
-            <!-- Muestra un mensaje de error si las credenciales son incorrectas -->
-            <?php if (isset($error_message)) { ?>
-              <div class="alert alert-danger" role="alert">
-                <?php echo $error_message; ?>
-              </div>
-            <?php } ?>
-    
-            <div class="d-flex justify-content-around align-items-center mb-4">
-              <!-- Checkbox -->
+<section class="vh-100">
+    <div class="container h-100">
+        <div class="row h-100 justify-content-center align-items-center">
+            <div class="col-md-6">
+                <div class="card shadow-lg">
+                    <div class="card-body">
+                        <h2 class="card-title text-center">Iniciar Sesión</h2>
+                        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+                            <div class="form-group">
+                                <label for="usuario">Nombre de usuario</label>
+                                <input type="text" id="usuario" class="form-control" name="usuario" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="contraseña">Contraseña</label>
+                                <input type="password" id="contraseña" class="form-control" name="contraseña" required>
+                            </div>
+                            <?php if (isset($error_message)) { ?>
+                                <div class="alert alert-danger" role="alert">
+                                    <?php echo $error_message; ?>
+                                </div>
+                            <?php } ?>
+                            <div class="text-center">
+                                <a href="register.php" class="btn btn-link">Registrarse</a>
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-block mt-3">Iniciar Sesión</button>
+                        </form>
+                    </div>
+                </div>
             </div>
-    
-            <div>
-              <a href="register.php">Register</a><br><br>
-              <input type="submit" value="Iniciar sesión">
+            <div class="col-md-6 d-none d-md-block">
+                <img src="../assets/img/LOGIN.jpg" class="img-fluid" alt="Phone image">
             </div>
-          </form>
         </div>
-      </div>
     </div>
-  </section>
+</section>
 
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
 </html>
--- paginas/register.php
+++ paginas/register.php
@@ -26,7 +26,7 @@ require '../conecciones/insercion.php'
             <div class="row justify-content-center">
               <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+                <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Registrese</p>
 
                 <form action="../conecciones/insercion.php"class="mx-1 mx-md-4" method="post">
 
@@ -34,7 +34,7 @@ require '../conecciones/insercion.php'
                     <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
                       <input type="text" id="form3Example1c" class="form-control" name="nombre" id="nombre" />
-                      <label class="form-label" for="form3Example1c" >Your Name</label>
+                      <label class="form-label" for="form3Example1c" >Nombre</label>
                     </div>
                   </div>
 
@@ -68,7 +68,7 @@ require '../conecciones/insercion.php'
                     <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
                       <input type="password" id="form3Example4c" class="form-control" name="contrasenia" id="contraseña" />
-                      <label class="form-label" for="form3Example4c" >Password</label>
+                      <label class="form-label" for="form3Example4c" >Contraseña</label>
                     </div>
                   </div>
 
@@ -85,7 +85,7 @@ require '../conecciones/insercion.php'
               </div>
               <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
 
-                <img src="../imagenes/mejor.png"
+                <img src="../assets/img/LOGIN.jpg"
                   class="img-fluid" alt="Sample image">
 
               </div>
